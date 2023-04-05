3d104f1e0bbfbf4eac883ea508912eda8d87536e
Anna Razina <vega_b@mail.ru>
2023-04-05 16:42:24 +0400

    начало работы над проэктом на react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Description from './component/description/Description';
+import Button from './component/button/Button';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Description/>
+      <Button>
+          CLICK ME!<br/>
+          Нажимай, и ты узнаешь<br/>
+          ВСЁ О СЕБЕ.<br/>
+          И немного обо мне.<br/>
+      </Button>
+      <Description/>
     </div>
   );
 }
---
+++ src/component/description/Description.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from "./Description.module.css";
+
+const  Description = () => {
+    return (
+      <p className={classes.description}>
+        надо придумать текст..
+      </p>
+    );
+  }
+  
+  export default Description;
