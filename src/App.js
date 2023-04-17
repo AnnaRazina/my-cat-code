cb89fe0bff099c8418fec604cbf61ec228982ef7
Anna Razina <vega_b@mail.ru>
2023-04-17 21:03:06 +0400

    верстка footer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import './App.css';
 import Description from './component/description/Description';
 import Button from './component/button/Button';
+import Footer from './component/footer/Footer';
 
 function App() {
   return (
     <div className='App'>
-      <Description/>
-      <Button>
-          CLICK ME!<br/>
-          Нажимай, и ты узнаешь<br/>
-          ВСЁ О СЕБЕ.<br/>
-          И немного обо мне.<br/>
-      </Button>
-      <Description/>
+      <main className='App_main'>
+        <Description/>
+        <Button>
+            CLICK ME!<br/>
+            Нажимай, и ты узнаешь<br/>
+            ВСЁ О СЕБЕ.<br/>
+            И немного обо мне.<br/>
+        </Button>
+        <Description/>
+      </main>
+      <Footer/>
     </div>
   );
 }
